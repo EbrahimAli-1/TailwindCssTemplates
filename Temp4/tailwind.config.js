79783a8e3626123f0bdd2acd8a04c2deac6a2e6d
Ebrahim Ali <93480401+EbrahimAli-1@users.noreply.github.com>
2024-02-18 22:05:55 +0200

    Add files via upload

---
+++ Temp4/tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          softOrange: "hsl(35, 77%, 62%)",
+          softRed: "hsl(5, 85%, 63%)",
+        },
+        neutral: {
+          offWhite: "hsl(36, 100%, 99%)",
+          grayishBlue: "hsl(233, 8%, 79%) ",
+          darkGrayishBlue: "hsl(236, 13%, 42%)",
+          veryDarkBlue: "hsl(240, 100%, 5%)",
+        },
+      },
+      fontFamily: {
+        Inter: ["Inter"],
+      },
+      boxShadow: {
+        inside: "inset 5px 4px 8px 0 rgb(0 0 0 / 0.1)",
+        outside: "3px 2px 5px -4px #2d2d2d",
+      },
+      fontSize: {
+        "15px": "15px",
+      },
+      letterSpacing: {
+        0.2: "0.2em",
+      },
+      spacing: {
+        "45px": "45px",
+      }
+    },
+  },
+  plugins: [],
+};
