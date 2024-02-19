294f7d1b36d4183b259d5ec7d243c76e70952700
Ebrahim Ali <93480401+EbrahimAli-1@users.noreply.github.com>
2024-02-20 00:02:53 +0200

    Add files via upload

--- Temp4/dist/js/main.js
+++ Temp4/dist/js/main.js
@@ -3,16 +3,18 @@ let links = document.getElementById("links");
 let close = document.getElementById("close");
 
 menu.onclick = function () {
-  links.classList.remove("h-0");
-  links.classList.add("h-fit");
+  links.classList.remove("-top-52");
+  links.classList.add("top-0");
+  links.classList.add("relative");
 
   close.classList.remove("hidden");
   menu.classList.add("hidden");
 };
 
 close.onclick = function () {
-  links.classList.add("h-0");
-  links.classList.remove("h-fit");
+  links.classList.add("-top-52");
+  links.classList.remove("top-0");
+  links.classList.remove("relative");
 
   close.classList.add("hidden");
   menu.classList.remove("hidden");
