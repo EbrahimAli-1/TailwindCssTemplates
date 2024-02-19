8368b11013848a3db18b99bbf138ca1080c8c68f
Ebrahim Ali <93480401+EbrahimAli-1@users.noreply.github.com>
2024-02-19 03:31:48 +0200

    Add files via upload

--- Temp4/dist/js/main.js
+++ Temp4/dist/js/main.js
@@ -2,37 +2,17 @@ let menu = document.getElementById("menu");
 let links = document.getElementById("links");
 let close = document.getElementById("close");
 
-// menu.onclick = function () {
-//   links.classList.remove("hidden");
-//   close.classList.remove("hidden");
-//   menu.classList.add("hidden");
-// };
-
-// close.onclick = function () {
-//   links.classList.add("hidden");
-//   close.classList.add("hidden");
-//   menu.classList.remove("hidden");
-// };
-
 menu.onclick = function () {
-  links.classList.remove("aboslute");
-  links.classList.add("relative");
-  links.classList.remove("-top-52");
-  links.classList.add("top-0");
-  links.classList.remove("-z-10");
-  links.classList.add("-z-0");
+  links.classList.remove("h-0");
+  links.classList.add("h-fit");
 
   close.classList.remove("hidden");
   menu.classList.add("hidden");
 };
 
 close.onclick = function () {
-  links.classList.add("aboslute");
-  links.classList.remove("relative");
-  links.classList.add("-top-52");
-  links.classList.remove("top-0");
-  links.classList.add("-z-10");
-  links.classList.remove("-z-0");
+  links.classList.add("h-0");
+  links.classList.remove("h-fit");
 
   close.classList.add("hidden");
   menu.classList.remove("hidden");
--- Temp4/tailwind.config.js
+++ Temp4/tailwind.config.js
@@ -1,36 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./dist/*.{html, js}"],
   theme: {
     extend: {
       colors: {
-        primary: {
-          softOrange: "hsl(35, 77%, 62%)",
-          softRed: "hsl(5, 85%, 63%)",
-        },
         neutral: {
-          offWhite: "hsl(36, 100%, 99%)",
-          grayishBlue: "hsl(233, 8%, 79%) ",
-          darkGrayishBlue: "hsl(236, 13%, 42%)",
-          veryDarkBlue: "hsl(240, 100%, 5%)",
+          almostWhite: "hsl(0, 0%, 98%)",
+          mediumGray: "hsl(0, 0%, 41%)",
+          almostBlack: "hsl(0, 0%, 8%)",
         },
       },
       fontFamily: {
-        Inter: ["Inter"],
+        Epilogue: "Epilogue",
+      },
+      fontSize: {
+        "18px": "18px",
       },
       boxShadow: {
         inside: "inset 5px 4px 8px 0 rgb(0 0 0 / 0.1)",
         outside: "3px 2px 5px -4px #2d2d2d",
       },
-      fontSize: {
-        "15px": "15px",
-      },
-      letterSpacing: {
-        0.2: "0.2em",
-      },
-      spacing: {
-        "45px": "45px",
-      }
     },
   },
   plugins: [],
